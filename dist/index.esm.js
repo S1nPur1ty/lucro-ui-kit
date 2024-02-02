d52a0e8c62bd1865ce573bbdc1d5b5e2495bfd77
likechallenge <102867163+IvanDragacevac@users.noreply.github.com>
2024-02-03 01:49:28 +1000

    update logo

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -705,36 +705,13 @@ var Icon$1n = function (props) {
 };
 
 var Icon$1m = function (props) {
-    return (
-    // <Svg viewBox="0 0 32 32" {...props}>
-    //   <path
-    //     fillRule="evenodd"
-    //     clipRule="evenodd"
-    //     d="M5.84199 5.00181C5.35647 2.40193 7.35138 0 9.9962 0C12.3302 0 14.2222 1.89206 14.2222 4.22603V9.43607C14.806 9.39487 15.3992 9.37374 16 9.37374C16.5772 9.37374 17.1474 9.39324 17.709 9.43131V4.22603C17.709 1.89206 19.601 0 21.935 0C24.5798 0 26.5747 2.40193 26.0892 5.00181L24.9456 11.1259C28.8705 12.8395 31.8384 15.8157 31.8384 19.5556V21.8182C31.8384 24.8936 29.8038 27.4686 26.9594 29.2068C24.0928 30.9586 20.2149 32 16 32C11.7851 32 7.90719 30.9586 5.04062 29.2068C2.19624 27.4686 0.161621 24.8936 0.161621 21.8182V19.5556C0.161621 15.8355 3.09899 12.8708 6.99084 11.1538L5.84199 5.00181ZM23.48 11.9305L24.8183 4.76446C25.1552 2.96 23.7707 1.29293 21.935 1.29293C20.3151 1.29293 19.0019 2.60612 19.0019 4.22603V10.8562C18.5774 10.8018 18.1462 10.7586 17.709 10.7274C17.1484 10.6873 16.5782 10.6667 16 10.6667C15.3982 10.6667 14.8049 10.689 14.2222 10.7324C13.785 10.765 13.3537 10.8094 12.9293 10.8651V4.22603C12.9293 2.60612 11.6161 1.29293 9.9962 1.29293C8.16055 1.29293 6.77597 2.96 7.11295 4.76446L8.45562 11.9543C4.25822 13.5135 1.45455 16.3344 1.45455 19.5556V21.8182C1.45455 26.7274 7.96677 30.7071 16 30.7071C24.0332 30.7071 30.5455 26.7274 30.5455 21.8182V19.5556C30.5455 16.318 27.7131 13.4847 23.48 11.9305Z"
-    //     fill="#633001"
-    //   />
-    //   <path
-    //     d="M30.5455 21.8183C30.5455 26.7275 24.0333 30.7072 16 30.7072C7.96681 30.7072 1.45459 26.7275 1.45459 21.8183V19.5557H30.5455V21.8183Z"
-    //     fill="#FEDC90"
-    //   />
-    //   <path
-    //     fillRule="evenodd"
-    //     clipRule="evenodd"
-    //     d="M7.11298 4.7645C6.77601 2.96004 8.16058 1.29297 9.99624 1.29297C11.6161 1.29297 12.9293 2.60616 12.9293 4.22607V10.8652C13.9192 10.7351 14.9466 10.6667 16 10.6667C17.0291 10.6667 18.0333 10.732 19.0019 10.8562V4.22607C19.0019 2.60616 20.3151 1.29297 21.935 1.29297C23.7707 1.29297 25.1553 2.96004 24.8183 4.7645L23.4801 11.9306C27.7131 13.4847 30.5455 16.318 30.5455 19.5556C30.5455 24.4648 24.0333 28.4445 16 28.4445C7.96681 28.4445 1.45459 24.4648 1.45459 19.5556C1.45459 16.3345 4.25826 13.5135 8.45566 11.9543L7.11298 4.7645Z"
-    //     fill="#D1884F"
-    //   />
-    //   <path
-    //     className="left-eye"
-    //     d="M11.9595 18.9091C11.9595 20.248 11.2359 21.3333 10.3433 21.3333C9.45075 21.3333 8.72717 20.248 8.72717 18.9091C8.72717 17.5702 9.45075 16.4849 10.3433 16.4849C11.2359 16.4849 11.9595 17.5702 11.9595 18.9091Z"
-    //     fill="#633001"
-    //   />
-    //   <path
-    //     className="right-eye"
-    //     d="M23.1111 18.9091C23.1111 20.248 22.3875 21.3333 21.4949 21.3333C20.6024 21.3333 19.8788 20.248 19.8788 18.9091C19.8788 17.5702 20.6024 16.4849 21.4949 16.4849C22.3875 16.4849 23.1111 17.5702 23.1111 18.9091Z"
-    //     fill="#633001"
-    //   />
-    // </Svg>
-    React__default.createElement("img", { src: "sweems.png", width: "48px" }));
+    return (React__default.createElement(Svg, { width: "40", height: "48", viewBox: "0 0 40 48", fill: "none", xmlns: "http://www.w3.org/2000/svg" },
+        React__default.createElement("path", { d: "M27.5965 29.4033L32.3965 32.5001L26.4001 36.3873L21.6 33.2906L27.5965 29.4033Z", fill: "white" }),
+        React__default.createElement("path", { d: "M12.8 29.9032L8 33L13.6009 36.3872L18.4009 33.2904L12.8 29.9032Z", fill: "white" }),
+        React__default.createElement("path", { d: "M19.9992 34.0647L15.1992 37.1615L19.9992 40.1034L24.7992 37.1615L19.9992 34.0647Z", fill: "white" }),
+        React__default.createElement("path", { d: "M5.6 16.2581L0 12.3871L20 0L40 12.3871L34.4 16.2581L27.2 11.6129V27.871L20 32.5161L12.8 27.871V11.6129L5.6 16.2581Z", fill: "white" }),
+        React__default.createElement("path", { d: "M4.8 16.8773L0 13.5967V37.1612L19.2 47.9999V41.5L4.8 32.5161V16.8773Z", fill: "white" }),
+        React__default.createElement("path", { d: "M35.2 16.8773L40 13.5967V37.1612L20.8 47.9999V41.5L35.2 32.5161V16.8773Z", fill: "white" })));
 };
 
 var Logo$2 = function (_a) {
@@ -4912,7 +4889,7 @@ var links = [
 var MENU_HEIGHT = 56;
 
 var Wrapper = styled.div(templateObject_1$7 || (templateObject_1$7 = __makeTemplateObject(["\n  position: relative;\n  width: 100%;\n"], ["\n  position: relative;\n  width: 100%;\n"])));
-var StyledNav = styled.nav(templateObject_2$4 || (templateObject_2$4 = __makeTemplateObject(["\n  position: fixed;\n  top: ", ";\n  left: 0;\n  transition: top 0.2s;\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n  width: 100%;\n  height: ", "px;\n  background-color: ", ";\n  // border-bottom: 1px solid ", ";\n  z-index: 20;\n  transform: translate3d(0, 0, 0);\n\n  padding-left: 16px;\n  padding-right: 16px;\n"], ["\n  position: fixed;\n  top: ", ";\n  left: 0;\n  transition: top 0.2s;\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n  width: 100%;\n  height: ", "px;\n  background-color: ", ";\n  // border-bottom: 1px solid ", ";\n  z-index: 20;\n  transform: translate3d(0, 0, 0);\n\n  padding-left: 16px;\n  padding-right: 16px;\n"])), function (_a) {
+var StyledNav = styled.nav(templateObject_2$4 || (templateObject_2$4 = __makeTemplateObject(["\n  position: fixed;\n  top: ", ";\n  left: 0;\n  transition: top 0.2s;\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n  width: 100%;\n  height: ", "px;\n  // background-color: ", ";\n  // border-bottom: 1px solid ", ";\n  z-index: 20;\n  transform: translate3d(0, 0, 0);\n\n  padding-left: 16px;\n  padding-right: 16px;\n"], ["\n  position: fixed;\n  top: ", ";\n  left: 0;\n  transition: top 0.2s;\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n  width: 100%;\n  height: ", "px;\n  // background-color: ", ";\n  // border-bottom: 1px solid ", ";\n  z-index: 20;\n  transform: translate3d(0, 0, 0);\n\n  padding-left: 16px;\n  padding-right: 16px;\n"])), function (_a) {
     var showMenu = _a.showMenu;
     return (showMenu ? 0 : "-" + MENU_HEIGHT + "px");
 }, MENU_HEIGHT + 1, function (_a) {
